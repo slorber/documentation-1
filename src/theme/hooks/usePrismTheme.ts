99eeab942748ef055d0c3ac7cda097a74e7db102
Benno Kohrs <bennokohrs@gmail.com>
2021-02-22 19:36:40 +0100

    used useThemeContext instead of agileTs core theme

--- src/theme/hooks/usePrismTheme.ts
+++ src/theme/hooks/usePrismTheme.ts
@@ -1,16 +1,15 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { useAgile } from '@agile-ts/react';
-import core from '../../core';
 import defaultTheme from 'prism-react-renderer/themes/palenight';
+import useThemeContext from '@theme/*';
 
 const usePrismTheme = () => {
   const { siteConfig } = useDocusaurusContext();
   const prismTheme = siteConfig.themeConfig.prism;
   const lightModeTheme = prismTheme.theme || defaultTheme;
   const darkModeTheme = prismTheme.darkTheme || lightModeTheme;
-  const themeType = useAgile(core.ui.THEME_TYPE);
+  const { isDarkTheme } = useThemeContext();
 
-  return themeType === 'dark' ? darkModeTheme : lightModeTheme;
+  return isDarkTheme ? darkModeTheme : lightModeTheme;
 };
 
 export default usePrismTheme;
